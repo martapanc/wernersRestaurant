f8d85b5db1c8fed82fb7585ca709685c53ae32be
Marta Pancaldi <marta.panc@gmail.com>
2019-11-23 20:46:33 +0000

    Add static content and mapping

--- src/main/java/com/example/restaurant/RestaurantApplication.java
+++ src/main/java/com/example/restaurant/RestaurantApplication.java
@@ -2,9 +2,15 @@ package com.example.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class RestaurantApplication {
+
+    @RequestMapping("/")
+    public String index() {
+        return "greeting";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantApplication.class, args);
---
+++ src/main/java/com/example/restaurant/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.restaurant;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/images/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/static/assets/imgages/",
+                        "classpath:/static/assets/css/",
+                        "classpath:/static/assets/js/");
+    }
+
+}
