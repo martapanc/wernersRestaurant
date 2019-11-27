a8d7bb8bb14aaae8c6437d5a1a058503690813ec
Marta Pancaldi <marta.panc@gmail.com>
2019-11-27 00:47:18 +0000

    Add find, create and edit endpoints for item class

--- src/main/java/com/example/restaurant/controllers/FoodClassController.java
+++ src/main/java/com/example/restaurant/controllers/FoodClassController.java
@@ -1,10 +1,18 @@
 package com.example.restaurant.controllers;
 
+import com.example.restaurant.entity.FoodClass;
 import com.example.restaurant.utils.Utils;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 import static com.example.restaurant.utils.Utils.JSON_UTF_8;
 
@@ -17,5 +25,40 @@ public class FoodClassController {
     @ResponseBody
     public String listFoodClasses() {
         return Utils.readInput(FOOD_CLASS_JSON);
+    }
+
+    @GetMapping("/foodClassCRUD")
+    @ResponseBody
+    public String foodClassCRUD() {
+        return "dashboard/crud/foodClassCRUD";
+    }
+
+    @RequestMapping(value = "/foodClass-find", method = RequestMethod.POST)
+    public String findFoodClass(@RequestParam String id, HttpSession session) {
+        FoodClass foodClass = ("0".equals(id) ? new FoodClass() : find(id));
+        session.setAttribute("fc", foodClass);
+
+        return "dashboard/crud/foodClassCRUD";
+    }
+
+    private FoodClass find(String id) {
+        Gson gson = new Gson();
+        long longId = (id == null) ? 0 : Long.parseLong(id);
+
+        List<FoodClass> foodClassList = gson.fromJson(Utils.readInput(FOOD_CLASS_JSON), new TypeToken<List<FoodClass>>() {}.getType());
+        return foodClassList.stream().filter(fc -> fc.getId() == longId).findFirst().orElse(null);
+    }
+
+    @RequestMapping(value = "/foodClassAction", method = RequestMethod.POST, produces = JSON_UTF_8)
+    @ResponseBody
+    public void foodClassAction(@RequestParam String action, @RequestParam String id) {
+
+        switch (action) {
+            case "edit":
+            case "find":
+            case "create":
+            case "delete":
+                System.out.println(action);
+        }
     }
 }
---
+++ src/main/resources/static/assets/js/tables/crud.js
@@ -0,0 +1,142 @@
+/**
+ * This js file handles the CRUD actions of the bootstrap table by adding event
+ * handlers to the buttons, controling modal visualization, notifications for
+ * feedback and handling error situations.
+ * 
+ */
+
+var $table = $('#table');
+
+var $cButton = $('#create');
+var $cModal = $('#create-modal');
+var $cBody = $('#create-modal-body');
+
+var $eButton = $('.edit');
+var $eModal = $('#edit-modal');
+var $eBody = $('#edit-modal-body');
+
+var $delete = $('#delete');
+var $modals = $('.modal');
+
+var $notifier = $('#notify');
+
+/**
+ * Initializes the webpage for CRUD operations. Add eventhandlers to create,
+ * edit and delete button of bootstrap table
+ * 
+ */
+function initCRUD(API_URL) {
+	
+	// create button 
+	$cButton.on('click', function(){
+	    $cBody.load(API_URL + "-find" + ' #create-form', {action: 'find', id: 0}, function (responseText, textStatus, req) {
+	    	if(textStatus == 'error') {
+	    		$notifier.notify({
+				    message: { text: 'Server error while sending a find request!' },
+				    type: 'danger',
+				    closable: false,
+				  }).show(); 
+	    	} else {
+	    		console.log('Successfully loaded create-form');
+	    		$('#create-form').validator().on('submit', function(e) {
+	    			if (e.isDefaultPrevented()) {
+	    			    console.log('Create form validation has failed');
+	    			  } else {
+	    				e.preventDefault();
+						sendCRUDRequest(API_URL, $(this).serialize());
+	    			  }
+	    		});
+			    	$cModal.modal();
+	    	}
+		});
+	});
+	
+	// edit button (window.action events is needed because of async table creation)
+	window.actionEvents = {
+		'click .edit' : function(e, value, row) {
+		    $eBody.load(API_URL + "-find" + ' #edit-form', {action: 'find', id: row.id}, function (responseText, textStatus, req) {
+		    	if(textStatus == 'error') {
+		    		$notifier.notify({
+					    message: { text: 'Server error while sending a find request!' },
+					    type: 'danger',
+					    closable: false,
+					  }).show();
+		    	} else {
+		    		console.log('Successfully loaded edit-form');
+		    		$('#edit-form').validator().on('submit', function(e) {
+		    			if (e.isDefaultPrevented()) {
+		    			    console.log('Edit form validation has failed');
+		    			  } else {
+		    				e.preventDefault();
+							sendCRUDRequest(API_URL, $(this).serialize());
+		    			  }
+		    		});
+			    	$eModal.modal();
+	            }
+			});
+		}
+	};
+			
+	// checkboxes in table (to enable delete button)
+	$table.on('check.bs.table uncheck.bs.table '
+			+ 'check-all.bs.table uncheck-all.bs.table', function() {
+					$delete.prop('disabled', !$table.bootstrapTable('getSelections').length);
+		// save your data, here just save the current page
+		// selections = getIdSelections();
+		// push or splice the selections if you want to save all data selections
+	});
+
+	
+	// delete button
+	$delete.on('click', function() {
+		var idArray = getIdSelections();
+		var formData = 'action=delete&id=' + idArray.join('&id=');
+		$('#delete-modal-body').html('<p>Are you sure to delete ' + idArray.length + ' entries?<p>');
+		$('#delete-modal').modal();
+		$('#delete-button').on('click', function() {
+				sendCRUDRequest(API_URL, formData);
+			  });
+	});
+};
+
+/**
+ * Sends an AJAX call based on the action parameter (which can be 
+ * either create, edit or delete), in order to interact with the
+ * database. After the call (no matter if succesful) all open modals 
+ * (in theory only one is open) are closed.
+ * 
+ * @param {String} 
+ * 				action String that defines the CRUD action
+ */
+function sendCRUDRequest(URL, formdata) {
+	console.log(URL, formdata);
+	var regex_id = /id=([a-z]+)/g;
+	var regex_action = /action=([a-z]+)/g;
+	var action = regex_action.exec(formdata);
+	var id = regex_id.exec(formdata);
+	$.ajax({
+		url : URL,
+		type : 'post',
+		data: formdata,
+		success : function() {
+
+			$('#notify').notify({
+			    message: { text: action[1].toUpperCase() + ' executed!' },
+			    type: 'success',
+			    closable: false,
+			  }).show();
+			$table.bootstrapTable('refresh');
+		},
+		error : function() {
+			console.log(URL, formdata);
+			$notifier.notify({
+			    message: { text: 'Server error while executing ' + action[1] + ' request!' },
+			    type: 'danger',
+			    closable: false,
+			  }).show();
+		}
+	});
+	$modals.modal('hide');
+};
+
+
