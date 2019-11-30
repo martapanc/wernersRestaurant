74414c8a4b0cd6168164287fcd304f510d35e5c1
Marta Pancaldi <marta.panc@gmail.com>
2019-11-30 23:19:56 +0000

    Add checkout methods and views

--- src/main/java/com/example/restaurant/controllers/TakeawayController.java
+++ src/main/java/com/example/restaurant/controllers/TakeawayController.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.HashSet;
@@ -63,4 +66,10 @@ public class TakeawayController {
         return new Gson().toJson(takeawayOrderList);
     }
 
+    @RequestMapping(value = "/create-takeaway", method = RequestMethod.POST)
+    @ResponseBody
+    public ModelAndView createTakeawayOrder(HttpServletRequest request, HttpSession session) {
+
+        return new ModelAndView("");
+    }
 }
--- src/main/resources/static/assets/js/takeaway/takeawayCheckoutUtil.js
+++ src/main/resources/static/assets/js/takeaway/takeawayCheckoutUtil.js
@@ -3,137 +3,65 @@ $(".date").html("Date: &ensp;<strong>" + moment().format('ddd DD MMM YYYY, hh:mm
 var cart = JSON.parse(localStorage.getItem("cart"));
 var totPrice = JSON.parse(localStorage.getItem("totPrice"));
 
-$(function() { // Display items in cart
-
-	cart.forEach(function(entry) {
-		$(".cart").append(
-				"<tr><td>" + entry.id + "</td><td>" + entry.name + "</td><td>"
-						+ entry.qnt + "</td><td>€ " + entry.totPrice.toFixed(2)
-						+ "</td></tr>");
-	});
-	$(".cart").append(
-			"<tr><td></td><td></td><th>Total: </th><td>€ "
-					+ totPrice.toFixed(2) + "</td></tr>");
-	// localStorage.clear();
+$(function () { // Display items in cart
+    cart.forEach(function (entry) {
+        $(".cart").append(
+            "<tr><td>" + entry.id + "</td><td>" + entry.name + "</td><td>"
+            + entry.qnt + "</td><td>€ " + entry.totPrice.toFixed(2)
+            + "</td></tr>");
+    });
+    $(".cart").append(
+        "<tr><td></td><td></td><th>Total: </th><td>€ "
+        + totPrice.toFixed(2) + "</td></tr>");
+    // localStorage.clear();
 });
 
-$('#reservation-form')
-		.bootstrapValidator(
-				{
-					trigger : 'blur',
-					fields : {
-						firstname : {
-							validators : {
-								notEmpty : {
-									message : 'Your first name is required'
-								},
-								regexp : {
-									regexp : /^[a-zA-Z ]+$/,
-									message : 'Your first name cannot have numbers or symbols'
-								}
-							}
-						},
-						lastname : {
-							validators : {
-								notEmpty : {
-									message : 'Your last name is required'
-								},
-								regexp : {
-									regexp : /^[a-zA-Z ]+$/,
-									message : 'Your last name cannot have numbers or symbols'
-								}
-							}
-						},
-						email : {
-							validators : {
-								regexp : {
-									regexp : /^(([^<>()\[\]\\.,;:\s@]+(\.[^<>()\[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-									message : 'The input is not a valid email address'
-								}
-							}
-						},
-						telephone : {
-							validators : {
-								notEmpty : {
-									message : 'The phone number is required'
-								},
-								regexp : {
-									regexp : /^[0-9]{10}$/,
-									message : 'The input is not a valid phone number (ex: 0123456789)'
-								}
-							}
-						},
-						address : {
-							validators : {
-								notEmpty : {
-									message : 'The delivery address is required'
-								},
-								regexp : {
-									regexp : /^[a-zA-Z0-9- ,.]{5,100}$/,
-									message : 'The input is not a valid address'
-								}
-							}
-						}
-					}
-				}).on('error.field.bv', '[name="phone"]', function(e, data) {
-			// change the data-bv-trigger value to keydown
-			$(e.target).attr('data-bv-trigger', 'keydown');
-			// destroy the plugin
-			// console.info(data.bv.getOptions());
-			data.bv.destroy();
-			// console.info(data.bv.getOptions());
-			// initialize the plugin
-			$('#reservation-form').bootstrapValidator(data.bv.getOptions());
-		});
+$("#submit-btn").click(function () {
+    var title = document.getElementById("title").value;
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var telephone = document.getElementById("telephone").value;
+    var email = document.getElementById("email").value;
+    var session = document.getElementById("session").value;
+    var address = document.getElementById("address").value;
+    var comment = document.getElementById("comments").value;
 
-$("#submit-btn")
-		.click(
-				function() {
-					var title = document.getElementById("title").value;
-					var firstname = document.getElementById("firstname").value;
-					var lastname = document.getElementById("lastname").value;
-					var telephone = document.getElementById("telephone").value;
-					var email = document.getElementById("email").value;
-					var session = document.getElementById("session").value;
-					var address = document.getElementById("address").value;
-					var comment = document.getElementById("comments").value;
+    $(".reservation-area").addClass("col-md-12").html(
+        "</br><strong>Reservation name:</strong>").append(
+        "&emsp;" + title + " " + firstname + " " + lastname
+        + "</br>").append(
+        "<strong>Telephone number: </strong>&emsp;"
+        + telephone + "</br>").append(
+        "<strong>Delivery address: </strong>&emsp;"
+        + address + "</br>");
+    if (email != "")
+        $(".reservation-area").append(
+            "<strong>Email address: </strong>&emsp;"
+            + email);
+    $(".reservation-area").append("</br>&emsp;");
 
-					$(".reservation-area").addClass("col-md-12").html(
-							"</br><strong>Reservation name:</strong>").append(
-							"&emsp;" + title + " " + firstname + " " + lastname
-									+ "</br>").append(
-							"<strong>Telephone number: </strong>&emsp;"
-									+ telephone + "</br>").append(
-							"<strong>Delivery address: </strong>&emsp;"
-									+ address + "</br>");
-					if (email != "")
-						$(".reservation-area").append(
-								"<strong>Email address: </strong>&emsp;"
-										+ email);
-					$(".reservation-area").append("</br>&emsp;");
-
-					$.post({
-						type : "POST",
-						url : "/restaurantProject/takeaway",
-						data : {
-							"cart" : localStorage.getItem("cart"),
-							"tot" : totPrice,
-							"title" : title,
-							"firstname" : firstname,
-							"lastname" : lastname,
-							"email" : email,
-							"telephone" : telephone,
-							"session" : session,
-							"address" : address,
-							"comment" : comment
-						},
-						complete : function(response) {
-							console.log(response);
-							alert("We are currently working on your reservation! You will receive a phone call as soon as we are ready to deliver it.");
-						},
-						error : function(error) {
-							console.log("error" + error);
-						}
-					});
-					return false;
-				});
+    $.post({
+        type: "POST",
+        url: "/create-takeaway",
+        data: {
+            "cart": localStorage.getItem("cart"),
+            "tot": totPrice,
+            "title": title,
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": email,
+            "telephone": telephone,
+            "session": session,
+            "address": address,
+            "comment": comment
+        },
+        complete: function (response) {
+            console.log(response);
+            alert("We are currently working on your reservation! You will receive a phone call as soon as we are ready to deliver it.");
+        },
+        error: function (error) {
+            console.log("error" + error);
+        }
+    });
+    return false;
+});
